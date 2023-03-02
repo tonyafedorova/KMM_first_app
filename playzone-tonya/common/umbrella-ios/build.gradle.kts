6a117110582c387701e1e1cac93e6aa3304da333
a.fedorova <a.fedorova@qiwi.com>
2023-03-02 22:36:32 +0300

    Add podfile

--- playzone-tonya/common/umbrella-ios/build.gradle.kts
+++ playzone-tonya/common/umbrella-ios/build.gradle.kts
@@ -1,4 +1,28 @@
 plugins {
     id("multiplatform-setup")
     id("android-setup")
+    kotlin("native.cocoapods")
+}
+
+version = "0.0.1"
+
+kotlin {
+    cocoapods {
+        summary = "PlayZone iOS SDK"
+        homepage = "https://github.com/tonyafedorova/KMM_first_app"
+        ios.deploymentTarget = "12.0"
+
+        framework {
+            transitiveExport = false
+            baseName = "SharedSDK"
+            export(project(":common:core"))
+            export(project(":common:core-utils"))
+            export(project(":common:auth:api"))
+            export(project(":common:auth:presentation"))
+            export(project(":common:games:api"))
+            export(project(":common:games:presentation"))
+            export(project(":common:tournaments:api"))
+            export(project(":common:tournaments:presentation"))
+        }
+    }
 }
